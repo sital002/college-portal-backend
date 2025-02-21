add513fed7896e883a401f23a501301c76e5e70b
sital002 <sitaladhikari002@gmail.com>
2025-02-21 21:20:38 +0545

    feat: handle validation error

--- src/main/java/com/example/CollegeBackend/CollegeBackendApplication.java
+++ src/main/java/com/example/CollegeBackend/CollegeBackendApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @SpringBootApplication
@@ -17,9 +22,20 @@ public class CollegeBackendApplication {
 	}
 }
 @ControllerAdvice
-class ValidationHandler{
+class GlobalExceptionHandler{
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errorDetails = new HashMap<>();
 
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errorDetails.put(fieldName, errorMessage);
+		});
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
+	}
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ApiResponse> handleGeneralException(Exception ex) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(false,ex.getMessage()));
--- src/main/java/com/example/CollegeBackend/model/ApiResponse.java
+++ src/main/java/com/example/CollegeBackend/model/ApiResponse.java
@@ -13,4 +13,5 @@ public class ApiResponse {
         this.success = success;
         this.message = message;
     }
+
 }
