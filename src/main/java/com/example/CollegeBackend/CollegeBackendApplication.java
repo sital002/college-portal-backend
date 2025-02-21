9e349e381958ac608863dea16153be0770222f11
sital002 <sitaladhikari002@gmail.com>
2025-02-21 21:38:10 +0545

    feat: handle validation error

--- src/main/java/com/example/CollegeBackend/CollegeBackendApplication.java
+++ src/main/java/com/example/CollegeBackend/CollegeBackendApplication.java
@@ -1,14 +1,16 @@
 package com.example.CollegeBackend;
 
 import com.example.CollegeBackend.model.ApiResponse;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,23 +23,40 @@ public class CollegeBackendApplication {
 		SpringApplication.run(CollegeBackendApplication.class, args);
 	}
 }
-@ControllerAdvice
-class GlobalExceptionHandler{
+@RestControllerAdvice
+class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+	public ResponseEntity<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
 		Map<String, String> errorDetails = new HashMap<>();
-
 		ex.getBindingResult().getAllErrors().forEach(error -> {
 			String fieldName = ((FieldError) error).getField();
 			String errorMessage = error.getDefaultMessage();
 			errorDetails.put(fieldName, errorMessage);
 		});
 
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
+		ApiError apiError = new ApiError(errorDetails);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ApiError> handleIllegalArgumentException(IllegalArgumentException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError(ex.getMessage()));
 	}
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ApiResponse> handleGeneralException(Exception ex) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(false,ex.getMessage()));
+	public ResponseEntity<ApiError> handleException(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiError(ex.getMessage()));
+	}
+
+}
+
+@Getter
+@Setter
+class ApiError {
+	private Object error;
+	private boolean success;
+	public ApiError(Object error) {
+		this.error = error;
+		this.success = false;
 	}
 }
--- src/main/java/com/example/CollegeBackend/model/ApiResponse.java
+++ src/main/java/com/example/CollegeBackend/model/ApiResponse.java
@@ -8,10 +8,18 @@ import lombok.Setter;
 public class ApiResponse {
     private boolean success;
     private String message;
+    private Object data;
 
     public ApiResponse(boolean success, String message) {
         this.success = success;
         this.message = message;
+        this.data = null;
+    }
+
+    public ApiResponse( boolean success ,String message,Object data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
     }
 
 }
