14619aa5f289e2c5d3f608396ffaa6fd6245058e
sital002 <sitaladhikari002@gmail.com>
2025-03-04 19:46:45 +0545

    fix: cors error

--- src/main/java/com/example/CollegeBackend/config/Config.java
+++ src/main/java/com/example/CollegeBackend/config/Config.java
@@ -14,7 +14,7 @@ public class Config {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:8081")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
